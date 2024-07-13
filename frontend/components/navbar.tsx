fdd76d4e9e38f34dcceca5130dd1efcaaaf4d2b5
Paul Geeser <commit@pauldev.sh>
2024-07-13 22:47:24 +0200

    implemented apemode

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,19 +1,88 @@
+"use client";
 import { Link } from "@nextui-org/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
 import { title } from "./primitives";
 
 import { siteConfig } from "@/config/site";
+import apemode from "@/public/apemode.gif";
+import { useEffect, useState } from "react";
 
 const Navbar: React.FC = () => {
+  const [startApemode, setStartApemode] = useState(false);
+  const [raindropTimeout, setRaindropTimeout] =
+    useState<NodeJS.Timeout | null>();
+  const { setTheme } = useTheme();
+
+  const createRaindrop = () => {
+    var raindrop = document.createElement("div");
+
+    raindrop.className = "raindrop";
+    // Set random size
+    var sizes = [8, 16, 32];
+    var size = sizes[Math.floor(Math.random() * sizes.length)];
+
+    raindrop.style.width = size + "px";
+    raindrop.style.height = size + "px";
+
+    // Set random position
+    raindrop.style.left = Math.random() * window.innerWidth + "px";
+    raindrop.style.top = -size + "px";
+
+    // Set random animation duration
+    raindrop.style.animationDuration = 2 + Math.random() * 3 + "s";
+
+    document.body.appendChild(raindrop);
+
+    // Remove raindrop after animation ends
+    raindrop.addEventListener("animationend", function () {
+      document.body.removeChild(raindrop);
+    });
+}
+
+  useEffect(() => {
+    const backgroundMusic = document.getElementById(
+      "backgroundMusic",
+    ) as HTMLAudioElement;
+
+    if (startApemode) {
+      setTheme("dark");
+      backgroundMusic.play();
+      setRaindropTimeout(setInterval(createRaindrop, 200));
+    } else {
+      backgroundMusic.pause();
+      backgroundMusic.currentTime = 0;
+      clearInterval(raindropTimeout as NodeJS.Timeout);
+      document.querySelectorAll(".raindrop").forEach(function (raindrop) {
+        raindrop.remove();
+      });
+      setTheme("light");
+    }
+  }, [startApemode]);
+
   return (
     <div className="flex flex-col justify-center items-center m-3">
-      <div className="inline-block max-w-lg text-center">
-        <h1 className={title({ color: "violet" })}>{siteConfig.name}</h1>
-        <br />
-        <span>websites regardless of your design experience.</span>
-      </div>
+      <audio loop id="backgroundMusic">
+        <source src="apemode.mp3" type="audio/mp3" />
+        <track kind="captions" />
+        Your browser does not support the audio element.
+      </audio>
+      <Image
+        alt="GorillaGamble"
+        className="object-cover rounded-xl cursor-pointer"
+        src={apemode}
+        width={450}
+        onClick={() => setStartApemode(!startApemode)}
+      />
       <span>
-        <Link href="/">Voting</Link> · <Link href="/leaderboard">Leaderboard</Link>
+        <Link href="/" size="lg">
+          Voting
+        </Link>{" "}
+        ·{" "}
+        <Link href="/leaderboard" size="lg">
+          Leaderboard
+        </Link>
       </span>
     </div>
   );
