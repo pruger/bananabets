8cec9031bc2bb963a5174cd8763d64a94a9814ea
Paul Geeser <commit@pauldev.sh>
2024-07-13 21:47:45 +0200

    trying to fix ios zoom bug

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -22,6 +22,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export const viewport: Viewport = {
